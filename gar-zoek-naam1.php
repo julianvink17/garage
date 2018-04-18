7886b558aba67d3c81df2b9354689df57850202c
Julianvink17 <julian-_-vink@hotmail.com>
2018-04-18 12:21:19 +0200

    6 commit

---
+++ gar-zoek-naam1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>gar-zoek-naam1.php</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Garage zoek op klantnaam 1</h1>
+
+<p>
+    Dit formulier zoekt een klant op uit
+    de tabel klanten van database garage.
+</p>
+<form action="gar-zoek-naam2.php" method="post">
+    Welk klantnaam zoekt u?
+    <input type="text" name="klantnaamvak"> <br/>
+    <input type="submit">
+</form>
+</body>
+
+</html>
---
+++ gar-zoek-naam2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-zoek-naam2.php</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Imprima" rel="stylesheet">
+</head>
+<body>
+<h1>Garage zoek op klantid 2</h1>
+<p>
+    Op klantid gegevens zoeken uit de
+    tabel klaten van de database garage.
+</p>
+<?php
+error_reporting(0);
+$klantnaam = $_POST["klantnaamvak"];
+
+require_once "gar-connect.php";
+
+$sql = $conn->prepare("select klantid, klantnaam, klantadres, klantpostcode, klantplaats, klantemail from klant where klantnaam = :klantnaam");
+
+$sql->execute(["klantnaam" => $klantnaam]);
+
+echo "<a href='index.html'> Terug naar het menu </a>";
+
+
+echo "<table border='1px' style= 'font-family: Imprima, sans-serif'>";
+echo "<tr style='border-color: #09810c'>";
+echo "<th style='color: red'>ID</th>";
+echo "<th style='color: #294181'>Naam</th>";
+echo "<th style='color: #294181'>Adres</th>";
+echo "<th style='color: #294181'>Postcode</th>";
+echo "<th style='color: #294181'>Plaats</th>";
+echo "<th style='color: #294181'>Email</th>";
+echo "</tr>";
+
+foreach ($sql as $rij) {
+    echo "<tr style='text-align: center'>";
+    echo "<td style='color: red'>" . $rij["klantid"] . "</td>";
+    echo "<td>" . $rij["klantnaam"] . "</td>";
+    echo "<td>" . $rij["klantadres"] . "</td>";
+    echo "<td>" . $rij["klantpostcode"] . "</td>";
+    echo "<td>" . $rij["klantplaats"] . "</td>";
+    echo "<td>" . $rij["klantemail"] . "</td>";
+    echo "</tr>";
+    $value = $rij["klantid"];
+}
+echo "</table><br/>";
+
+if($value > 0) {
+    echo "GEVONDEN!";
+}
+else{
+    echo "NAAM NIET GEVONDEN!";
+
+}
+
+?>
+</body>
+</html>
+
+
